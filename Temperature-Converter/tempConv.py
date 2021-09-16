17e164c9d2b6e005c9df2f784ae881f737fb3979
TonySaco <tony.saco@yahoo.com>
2021-09-15 23:50:54 -0400

    temp converter v1, basic text program

--- Temperature-Converter/tempConv.py
+++ Temperature-Converter/tempConv.py
@@ -1,1 +1,27 @@
-print("Hello World")
+import sys
+
+# Convert Fahrenheit to Celsius
+def F_to_C():
+    input_F = input("Enter Fahrenheit value: ")
+    F_Conv = (float(input_F)-32.0)*(5/9)
+    C_Out = str(F_Conv) + '°C'
+    print(C_Out)
+
+# Convert Celsius to Fahrenheit
+def C_to_F():
+    input_C = input("Enter Celsius value: ")
+    F_Conv = (float(input_C)*(9/5))+32.0
+    F_Out = str(F_Conv) + "°F"
+    print(F_Out)
+
+# Execute the program
+def start():
+    choice = input("Enter F for Fahrenheit or C for Celsius: ")
+    if choice == "F" or choice == "f":
+        F_to_C()
+    elif choice == "C" or choice == "c":
+        C_to_F()
+    else:
+        sys.exit() # End program if choice is invalid
+
+start()
